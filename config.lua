8be5ab6c0820fc37195afa19422210cc1ebd8fec
Parkproductions <declan.holt@fitcollege.edu.au>
2020-04-19 12:48:05 +1200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,33 @@
+Config = {}
+
+Config.Usable = {"wood" , "tent", "kit_camp_simple", "pot", "cuffs", "water", "bread", "bread_bait","worms_bait","corn_bait", "cigar", "cigarettes", "beer"}
+
+Config.Labels = {
+  ["wood"] = "Wood Logs",
+  ["kit_camp_simple"] = "Camp Fire" ,
+  ["smallfish"] = "Small Fish",
+  ["mediumfish"] = "Medium Fish", 
+  ["largefish"] = "Large Fish",
+  ["epicfish"] = "Epic Fish", 
+  ["legendaryfish"] = "Legendary Fish",
+  ["bread_bait"] = "Bread Bait",
+  ["worms_bait"] = "Worms Bait", 
+  ["corn_bait"] = "Corn Bait",
+}
+
+Config.custom_names = false
+
+Config.Limit = {
+  ["wood"] = 10, 
+  ["kit_camp_simple"] = 2,
+  ["smallfish"] = 20,
+  ["mediumfish"] = 13, 
+  ["largefish"] = 7,
+  ["epicfish"] = 5, 
+  ["legendaryfish"] = 2,
+  ["bread_bait"] = 15,
+  ["worms_bait"] = 10, 
+  ["corn_bait"] = 10,
+}
+
+Pickups = {} 
